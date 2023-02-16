68047968eb0ced3afa7235cd08aa7045300d49b5
Marina Marković <m996markovic@gmail.com>
2023-02-16 21:31:09 +0100

    Added another pdf and regex for serbian latin alphabet.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import fnmatch
 import fitz
 import re
 
+import gensim
+from gensim.models import Word2Vec
+
+# Reading all PDF files from 'books' directory and storing it in text variable as string
 text = ''
 for file in os.listdir('books'):
     if fnmatch.fnmatch(file, '*.pdf'):
@@ -12,13 +16,33 @@ for file in os.listdir('books'):
             for page in doc:
                 text += page.getText()
 
-# words = text.split(' ')
+# Find all words in text and store it to the list of separate strings
 words = re.findall(r"[\w']+", text)
-print(len(words))
-# regex /[^a-zA-Za-åa-ö-w]/gi
-regex = re.compile(r"/[^a-zA-Za-åa-ö-w]/gi")
+# Here we have around 1436119 words
+# print(len(words))
+# Removing all duplicates from the list
 words = list(dict.fromkeys(words))
-print(len(words))
-# new_list_of_words = [i for i in words if not regex.search(i)]
+# Now we have 208710 words
+# print(len(words))
+
+# Creating regex for matching only words written in Serbian Latin alphabet and removing those that do not match it
+serbian_latin_alphabet = "abcdefghijklmnopqrstuvwxyzčćđšž"  # define the Serbian Latin alphabet
+regex = f'^[{serbian_latin_alphabet}{serbian_latin_alphabet.upper()}]+$'
+new_list_of_words = [i for i in words if re.match(regex, i)]
 # print(len(new_list_of_words))
+# Now there are 178127 unique words written strictly in Serbian Latin in the new_list_of_words array
+# (Numbers are only for reference and will change if new pdfs are added to the 'books' directory)
+# These words will be used as a dataset for neural networks
 # print(new_list_of_words)
+
+# Now that we have our dataset we can create vector using word2vec.
+# This vector will be used as input to our convolutional autoencoder
+
+# Create CBOW model
+model1 = gensim.models.Word2Vec(new_list_of_words, min_count=1, vector_size=len(new_list_of_words), window=5)
+
+# # Create Skip Gram model
+# model2 = gensim.models.Word2Vec(new_list_of_words, min_count=1, vector_size=len(new_list_of_words), window=5, sg=1)
+
+print(model1.wv.similarity('priča', 'ulica'))
+# print(model2.wv.similarity('priča', 'ulica'))
